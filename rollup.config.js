b2a499d1eb394e5a76efc8b6eaf30e98cd60acd3
ScottAgirs <scott@ijs.to>
2021-05-09 17:32:39 -0400

    feat(Rollup): Add @rollup/plugin-node-resolve
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,7 @@
 import { babel } from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
+// Will
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 // This will check all ESLinting while compiling.
 // Useful because will notify if anything should be fixed.
 import { eslint } from 'rollup-plugin-eslint';
@@ -16,8 +18,10 @@ export default {
     // https://github.com/rollup/plugins/tree/master/packages/babel#using-with-rollupplugin-commonjs
     commonjs(),
     babel({
+      babelHelpers: 'bundled',
       exclude: 'node_modules/**',
       presets: ['@babel/preset-env'],
     }),
+    nodeResolve(),
   ],
 };
